d28e8483ea05d5a3313dd909db3bcc313f09f56b
venkatasrinivas <pamidisrinivas4@gmail.com>
2025-06-18 19:36:42 +0530

    frontend development completed

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import { AuthProvider } from "../context/AuthContext.jsx";
+import { ChatProvider } from "../context/ChatContext.jsx";
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AuthProvider>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </AuthProvider>
+  </BrowserRouter>
+);
