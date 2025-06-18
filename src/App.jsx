0b9a473954b929fef77519795785aa6e447e7172
venkatasrinivas8688 <pamidisrinivas4@gmail.com>
2025-06-18 19:59:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { AuthContext } from "../context/AuthContext";
 const App = () => {
   const { authUser } = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-black">
       <Toaster />
       <Routes>
         <Route
